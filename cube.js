1a8a6d67f213ffb57b7ba158c4159c45fbda7ee9
vickocode <vicko.maris.septiadi@gmail.com>
2023-10-07 16:24:41 +0700

    pertama ya

--- cube.js
+++ cube.js
@@ -3,7 +3,7 @@ const input_number = 6;
 function printCube(input_number) {
   for (let i = 1; i <= input_number; i++) {
     const cube = Math.pow(i, 3);
-    console.log(`Current Number is: ${i} and the cube is: ${cube}`);
+    console.log(`Current Number is: ${i} and the cube is asdsads: ${cube}`);
   }
 }
 
