76e9fb63b58b1adfdbbab2fb3af3fe2f87a050bb
vickocode <vicko.maris.septiadi@gmail.com>
2023-10-07 16:30:17 +0700

    test push

--- cube.js
+++ cube.js
@@ -1,9 +1,9 @@
-const input_number = 6;
+const input_number = 5;
 
 function printCube(input_number) {
   for (let i = 1; i <= input_number; i++) {
     const cube = Math.pow(i, 3);
-    console.log(`Current Number is: ${i} and the cube is asdsads: ${cube}`);
+    console.log(`Current Number is: ${i} and the cube is: ${cube}`);
   }
 }
 
